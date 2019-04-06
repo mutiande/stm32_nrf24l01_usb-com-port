4f520476f26a410b6774fbc53d8e64033463bc57
Johan Svensson <johan@exclude.se>
2019-04-06 09:39:33 +0200

    First commit

---
+++ Debug/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.d
@@ -0,0 +1,92 @@
+Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.o: \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Inc/usbd_conf.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Inc/main.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Inc/stm32f1xx_hal_conf.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/core_cm3.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ /home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Inc/usbd_conf.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Inc/main.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Inc/stm32f1xx_hal_conf.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/core_cm3.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+/home/johan/Development/stm32/Projects/stm32_nrf24l01_usb-com-port/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
